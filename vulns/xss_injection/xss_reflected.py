828dbb84447aebe35364e9bdd84e04643df20faa
marl-ot <marlot.swyat@gmail.com>
2023-07-24 16:22:04 +0300

    update values in xss-injections htmls and connected forms for xss-injections to postgres db

--- vulns/xss_injection/xss_reflected.py
+++ vulns/xss_injection/xss_reflected.py
@@ -1,26 +1,33 @@
 from flask import render_template
+from database import execute_read
 
 
 def xss_reflected_page(request, app):
     search = request.args.get('search')
 
-    products = app.db_helper.execute_read(
-        f"SELECT * FROM products WHERE name LIKE :search",
+    persons = execute_read(
+        f"SELECT * FROM persons WHERE TRIM(login) LIKE '%{search}%'",
         { 'search': f'%{search}%' }
     )
 
-    products = list(
+    persons = list(
         map(
             lambda p: {
                 'id': p[0],
-                'name': p[1],
-                'price': p[2]
+                'login': p[1],
+                'password': p[2],
+                'passport_id': p[3],
+                'inn': p[4],
+                'phone_number': p[5],
+                'snils': p[6],
+                'email': p[7],
+                'person_role_id': p[8],
             },
-            products
+            persons
         )
     )
 
     return render_template(
         'xss_injection/xss-reflected.html',
-        products=products
+        persons=persons
     )
